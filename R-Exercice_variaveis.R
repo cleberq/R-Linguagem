4fb5fc41d5d0f9f8d117700085dc5b8f6a96db32
ClebersonMQ <57997050+cleberq@users.noreply.github.com>
2020-03-12 16:28:20 -0300

    up

---
+++ R-Exercice_variaveis.R
@@ -0,0 +1,13 @@
+# mi first code in R
+valorA <- 10
+valorB <- 20
+soma<-valorA + valorB
+soma
+### numeros
+valorA <- 40
+valorC <- "Helo World"
+class(valorA) #retorna que o tipo de dados é float (numeric)
+class(40L) #retorna que o tipo de dados inteiro
+class(valorC)
+
+
---
+++ R-Exercices_CodigosdeApoio.R
@@ -0,0 +1,14 @@
+## Comandos de apoio a codificação
+varA <- "ola"
+varB <- 5.50
+varC <- 10L #colocar L para inteiro
+varD <- "TRUE"
+
+# Verificar tipo de dado
+class(varA)
+class(varB)
+class(varC)
+class(varD)
+
+# Ajuda usar ? antes da função
+?class
